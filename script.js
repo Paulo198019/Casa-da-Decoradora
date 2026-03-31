41dde4fe569a472dcd45199bfc773f35f3d77702
Paulo198019 <pauloalexandremonteiro89@gmail.com>
2026-03-31 11:11:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
-let index = 0;
-
-function moveCarousel(step) {
-    const items = document.querySelectorAll(".carousel-item");
-    const slider = document.getElementById("carousel");
-
-    index += step;
-
-    if (index < 0) index = items.length - 1;
-    if (index >= items.length) index = 0;
-
-    slider.style.transform = `translateX(${-index * 100}%)`;
-}
-
-setInterval(() => moveCarousel(1), 3500);
-``
+// Ativa autoplay do carrossel
+const myCarousel = document.querySelector('#decorCarousel');
+const carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 2500,
+    ride: "carousel"
+});
