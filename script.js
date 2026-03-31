8d58d34bce6b09adb2b927a9a122bf6af192a87f
Paulo198019 <pauloalexandremonteiro89@gmail.com>
2026-03-31 12:15:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Ativa autoplay do carrossel
 const myCarousel = document.querySelector('#decorCarousel');
 const carousel = new bootstrap.Carousel(myCarousel, {
-    interval: 2800,
+    interval: 2500,
     ride: "carousel"
 });
